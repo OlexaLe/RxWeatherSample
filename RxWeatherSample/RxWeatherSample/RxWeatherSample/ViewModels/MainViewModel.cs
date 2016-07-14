2e66e6f51455122fe29c8205c83d60a19387355f
Olexandr Leuschenko <olexa.le@gmail.com>
2016-07-15 00:40:44 +0300

    ViewModel implemented

---
+++ RxWeatherSample/RxWeatherSample/RxWeatherSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using PropertyChanged;
+using Reactive.Bindings;
+using RxWeatherSample.Services;
+
+namespace RxWeatherSample.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class MainViewModel
+    {
+        private readonly OpenWeatherService _service = new OpenWeatherService();
+
+        public MainViewModel()
+        {
+            Temperature = City
+                .Throttle(TimeSpan.FromMilliseconds(500))
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(_service.GetTemperature)
+                .Switch()
+                .ToReadOnlyReactiveProperty("n/a");
+        }
+
+        public ReactiveProperty<string> City { get; } = new ReactiveProperty<string>();
+        public ReadOnlyReactiveProperty<string> Temperature { get; }
+    }
+}
